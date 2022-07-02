8ddca2a313eb46affe52f2866d66b5d82284efbc
Geryon04 <juanjavierzerofour@gmail.com>
2022-07-02 03:21:39 -0300

    first commit

--- bossEvent.js
+++ bossEvent.js
@@ -1,1 +1,6 @@
 //Crear un evento en que si el jugador hace clic en el boss, este se movera
+var boss = document.querySelector("#ball");
+
+boss.addEventListener("click", function(){
+    boss.classList.add("ball-move-click");
+});
